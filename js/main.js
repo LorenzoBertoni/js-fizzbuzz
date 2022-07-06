4ef129c48d79fb17c7a31c2f444f6ea8ce4e968b
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-06 15:41:04 +0200

    add: js, scritto il programma variabile in base alle condizioni prestabilite. style: stilizzati gli elementi creati da js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const wrapper = document.getElementById('wrapper');
+
+for (let i = 1; i <= 100; i++) {
+    let square = `<div class="square"> ${i} </div> `;
+    let fizz = '<div class="square fizz"> Fizz </div>'
+    let buzz = '<div class="square buzz"> Buzz </div>'
+    let fizzBuzz = '<div class="square fizzBuzz"> FizzBuzz </div>'
+        if ((i % 3 == 0) && (i % 5 == 0)){
+            wrapper.innerHTML += fizzBuzz;
+        } else if (i % 3 == 0){
+            wrapper.innerHTML += fizz;
+        }else if (i % 5 == 0) {
+            wrapper.innerHTML += buzz;
+        } else {
+            wrapper.innerHTML += square;
+        }
+}
